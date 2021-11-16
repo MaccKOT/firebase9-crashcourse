e225c9e63fad28bcddcd21b753afb8ed446a86be
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-11-16 14:59:10 +0400

    Add Firestore connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import { firebaseConfig } from '../firebase';
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+
+// init Firestore services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'books');
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    // console.log(snapshot.docs);
+
+    //extract data from response
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({
+        ...doc.data(),
+        id: doc.id,
+      });
+    });
+
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 console.log('hello from index.js');
